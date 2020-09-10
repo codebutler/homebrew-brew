a99cc8d588a44f79aa83ffbc5061bf2ae170559f
Eric Butler <eric@level.com>
2020-09-10 16:47:56 -0400

    Added minikube@1.12.2

---
+++ Formula/minikube@1.12.2.rb
@@ -0,0 +1,37 @@
+class MinikubeAT1122 < Formula
+  desc "Run a Kubernetes cluster locally"
+  homepage "https://minikube.sigs.k8s.io/"
+  url "https://github.com/kubernetes/minikube.git",
+      tag:      "v1.12.2",
+      revision: "be7c19d391302656d27f1f213657d925c4e1cfc2"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes/minikube.git"
+
+  depends_on "go" => :build
+  depends_on "go-bindata" => :build
+  depends_on "kubernetes-cli"
+
+  def install
+    system "make"
+    bin.install "out/minikube"
+
+    output = Utils.safe_popen_read("#{bin}/minikube", "completion", "bash")
+    (bash_completion/"minikube").write output
+
+    output = Utils.safe_popen_read("#{bin}/minikube", "completion", "zsh")
+    (zsh_completion/"_minikube").write output
+  end
+
+  test do
+    output = shell_output("#{bin}/minikube version")
+    assert_match "version: v#{version}", output
+
+    (testpath/".minikube/config/config.json").write <<~EOS
+      {
+        "vm-driver": "virtualbox"
+      }
+    EOS
+    output = shell_output("#{bin}/minikube config view")
+    assert_match "vm-driver: virtualbox", output
+  end
+end
